17e7c4638a0695c7d2b33f609e27bd37e0aa08bb
tkgstrator <nasawake.am@gmail.com>
2021-03-17 20:08:41 +0900

    Initial Commit

---
+++ ikastagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ikastagram
+//
+//  Created by Devonly on 3/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ikastagram/ikastagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikastagramApp.swift
+//  ikastagram
+//
+//  Created by Devonly on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ikastagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
